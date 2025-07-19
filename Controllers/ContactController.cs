ffe833b2c56d10267a5dc84582092e9e70e0a61d
sadeghi <sadeghi85@hotmail.com>
2025-07-19 13:09:36 +0330

    .

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿
+
+using backend.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactDbContext _context;
+
+        public ContactController(IContactDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/contacts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblContact>>> GetContacts()
+        {
+            return await _context.TblContacts.ToListAsync();
+        }
+
+        // POST: api/contacts
+        [HttpPost]
+        public async Task<ActionResult<TblContact>> PostContact(TblContact contact)
+        {
+            _context.TblContacts.Add(contact);
+            await _context.SaveChangesAsync();
+
+            // Returns a 201 Created status with the new contact
+            return CreatedAtAction(nameof(GetContacts), new { id = contact.Id }, contact);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace backend
 {
     public class Program
@@ -6,6 +9,17 @@ namespace backend
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("ContactDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ContactDbContextConnection' not found.");
+
+            builder.Services.AddDbContext<ContactDbContext>(options =>
+            {
+                //options.UseSqlServer(coreConnectionString, o => o.UseCompatibilityLevel(120)); // 'Contains' compatibility in core 8.0
+
+                options.UseSqlServer(connectionString);
+            });
+
+            builder.Services.AddScoped<IContactDbContext, ContactDbContext>();
 
             // Add services to the container.
 
@@ -22,13 +36,27 @@ namespace backend
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
+
+            // This middleware serves files from wwwroot
+            app.UseStaticFiles();
+
+            app.UseRouting();
 
             app.UseAuthorization();
 
 
             app.MapControllers();
+
+            // This is a fallback for client-side routing.
+            // It ensures that any request that doesn't match an API route
+            // will be served the index.html file.
+            app.MapFallbackToFile("index.html");
 
             app.Run();
         }
